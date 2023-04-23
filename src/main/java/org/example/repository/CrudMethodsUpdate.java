c1b82a07f046dcabd3a91f52e1e755d516c8144a
Marcin <marcin@mail>
2023-04-24 01:27:38 +0200

    Add some new CRUD methods

---
+++ src/main/java/org/example/repository/CrudMethodsUpdate.java
@@ -0,0 +1,31 @@
+package org.example.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.Main;
+import org.example.entity.Users;
+
+public class CrudMethodsUpdate {
+
+    static Logger logger = LogManager.getLogger(Main.class);
+    static EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("unit");
+
+    static EntityManager em=entityManagerFactory.createEntityManager();
+
+    public static void UpdateUserName(long id, String newName){
+
+        em.getTransaction().begin();
+        Users user= em.find(Users.class,id);
+        user.setName(newName);
+        em.merge(user);
+        logger.info(user);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+
+}
